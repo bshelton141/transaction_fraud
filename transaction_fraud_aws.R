658318e7a0a7a6326791f6a68ec5b15202bc1432
bshelton <bshelton@lacare.org>
2019-05-08 10:25:11 -0700

    added enhancements

--- transaction_fraud_aws.R
+++ transaction_fraud_aws.R
@@ -2,17 +2,17 @@
 # Configure the environment
 # ======================================================
 
-# change this parameter to "no" if the user does not want to run
-# the random forest model computations in parallel on n - 1 machine cores
-allow_parallel_processing <- "yes"
 
 # Install and load required packages
 packages <- c("jsonlite",
               "dplyr",
               "caret",
-              "doParallel",
               "randomForest",
+              "pROC",
+              "DMwR",
+              "ROSE",
               "Matrix",
+              "data.table",
               "xgboost",
               "ggplot2",
               "e1071")
@@ -160,7 +160,7 @@ data1 <- data %>%
   # create a field to label a record with 1 when it either followed by a "REVERSAL" for the same transaction OR if it is a "REVERSAL" itself
   mutate(remove_id = ifelse((dplyr::lead(transactionKey,
                                          n = 1,
-                                         default = 0) == transactionKey
+                                         default = "NA") == transactionKey
                              & dplyr::lead(transactionType) == "REVERSAL")
                             | transactionType == "REVERSAL",
                             1, 0)) %>%
@@ -187,7 +187,7 @@ sum(data$transactionAmount) - sum(data1$transactionAmount)
 data2 <- data1 %>%
   mutate(transactionDateTime = as.POSIXct(strptime(gsub("T", " ", transactionDateTime), "%Y-%m-%d %H:%M:%OS")),
 # Calculate the number of minutes between swipes for the same transactionKey
-         time_from_previous = ifelse (dplyr::lag(transactionKey, n = 1, default = NA) == transactionKey,
+         time_from_previous = ifelse (dplyr::lag(transactionKey, n = 1, default = "NA") == transactionKey,
                                       round(as.numeric(transactionDateTime - dplyr::lag(transactionDateTime,
                                                                                         n = 1,
                                                                                         default = NA))/60),
@@ -229,7 +229,7 @@ data2 <- data2 %>%
   arrange(customerId, transactionDateTime) %>%
 # Identify the time between the last purchase and the current purchase
 # for the same `customerId`
-  mutate(time_from_previous = ifelse (dplyr::lag(customerId, n = 1, default = NA) == customerId,
+  mutate(time_from_previous = ifelse (dplyr::lag(customerId, n = 1, default = "NA") == customerId,
                                       round(as.numeric(transactionDateTime - dplyr::lag(transactionDateTime,
                                                                                         n = 1,
                                                                                         default = NA))/60),
@@ -241,12 +241,16 @@ data2 <- data2 %>%
          time_from_previous = ifelse (is.na(time_from_previous),
                                       mean(time_from_previous, na.rm = T),
                                       time_from_previous),
+# Calculate the dates
+         transaction_date = as.Date(as.character(substr(transactionDateTime, 1, 10))),
 # Calculate the time of day that the transactions occurred
-        time_of_day = as.numeric(format(transactionDateTime, "%H")) +
+         time_of_day = as.numeric(format(transactionDateTime, "%H")) +
                       as.numeric(format(transactionDateTime, "%M"))/60,
-        CustTimeZscore = (time_from_previous - mean(time_from_previous)) / sd(time_from_previous),
+         CustTimeZscore = (time_from_previous - mean(time_from_previous)) / sd(time_from_previous),
 # Calculate the hour of day that the transaction occurred
-        hour_of_day = as.character(floor(time_of_day)),
+         hour_of_day = ifelse(nchar(as.character(floor(time_of_day))) == 1,
+                              paste0("0", as.character(floor(time_of_day))),
+                              as.character(floor(time_of_day))),
 # Calculate the z-score of each member's `transactionAmount` to understand
 # relative distributions of transaction amounts for each member
          CustAmtZscore = ifelse(total_trans_count == 1,
@@ -289,6 +293,53 @@ data3 <- data2 %>%
   dplyr::inner_join(data2.cat.b, by = c("customerId", "merchantCategoryCode")) %>%
   dplyr::inner_join(data2.hour.b, by = c("customerId", "hour_of_day"))
 
+# ======================================================
+# Explore Data
+# ======================================================
+ts1 <- data3 %>%
+  group_by(transaction_date, isFraud) %>%
+  summarise(amounts = sum(transactionAmount),
+            transactions = n())
+ts2 <- ts1 %>%
+  group_by(transaction_date) %>%
+  summarise(total_daily_amounts = sum(amounts),
+            total_daily_transactions = sum(transactions))
+ts <- dplyr::left_join(ts1, ts2, by = "transaction_date") %>%
+  mutate(perc_total_amount = amounts / total_daily_amounts,
+         perc_total_transactions = transactions / total_daily_transactions)
+
+ts_plot <- ggplot(data = ts, aes(x = transaction_date, y = transactions, color = isFraud)) +
+  geom_line() +
+  facet_wrap(. ~ isFraud, scales = "free", ncol = 1)
+
+ts_perc_fraud <- ggplot(data = subset(ts, isFraud == TRUE),
+                        aes(x = transaction_date, y = perc_total_transactions)) +
+  geom_line()
+
+hours1 <- data3 %>%
+  group_by(hour_of_day, isFraud) %>%
+  summarise(amounts = sum(transactionAmount),
+            transactions = n())
+hours2 <- hour1 %>%
+  group_by(hour_of_day) %>%
+  summarise(total_hourly_amounts = sum(amounts),
+            total_hourly_transactions = sum(transactions))
+hours <- dplyr::left_join(hours1, hours2, by = "hour_of_day") %>%
+  mutate(perc_total_amount = amounts / total_hourly_amounts,
+         perc_total_transactions = transactions / total_hourly_transactions)
+
+hours_plot <- ggplot(data = hours, aes(x = hour_of_day, y = transactions, color = isFraud)) +
+  geom_col() +
+  facet_wrap(. ~ isFraud, scales = "free", ncol = 1)
+
+
+ggplot(data = data3, aes(x = as.factor(isFraud), y = transactionAmount)) +
+  geom_boxplot() + facet_wrap(. ~ merchantCategoryCode, scale = "free")
+
+
+
+
+
 
 # ======================================================
 # Initial Feature Selection
@@ -329,6 +380,11 @@ sapply(data4, function(x) sum(is.na(x)))
 # Turn the outcome into a 0/1 label
 data4$Class <- ifelse(data4$isFraud == "TRUE", "Fraud", "notFraud")
 data4$isFraud <- NULL
+
+data4[sapply(data4, is.character)] <- lapply(data4[sapply(data4, is.character)],
+                                             as.factor)
+data4[sapply(data4, is.logical)] <- lapply(data4[sapply(data4, is.logical)],
+                                            as.factor)
 
 # One-hot encode the categorical variables
 dummies <- dummyVars(" ~ .", data = data4[, -ncol(data4)])
@@ -398,7 +454,11 @@ test_data <- test_pca
 test_label <- test.a$Class
 
 # Remove all unnecessary objects from memory
-rm(list=setdiff(ls(), c("train_data", "test_data", "test_label")))
+rm(list=setdiff(ls(), c("data4",
+                        "train_index",
+                        "train_data",
+                        "test_data",
+                        "test_label")))
 
 
 # ======================================================
@@ -415,29 +475,29 @@ fitControl <- trainControl(
     summaryFunction = twoClassSummary, # results summary function
     sampling = "down")                 # under-sample the majority class w/in each cv split
 
-system.time(
-  rf_model_down <- train(Class ~ .,
-                         data = train_data,
-                         method ='rf',
-                         tuneLength = 5,
-                         trControl = fitControl,
-                         metric = "ROC")
-)
+# system.time(
+#   rf_model_down <- train(Class ~ .,
+#                          data = train_data,
+#                          method ='rf',
+#                          tuneLength = 5,
+#                          trControl = fitControl,
+#                          metric = "ROC")
+# )
 
 # Train a Random Forest model with SMOTE during resample
-set.seed(3219)
-fitControl$sampling = "smote"
+# set.seed(3219)
+# fitControl$sampling = "smote"
+#
+# system.time(
+#   rf_model_smote <- train(Class ~ .,
+#                          data = train_data,
+#                          method ='rf',
+#                          tuneLength = 5,
+#                          trControl = fitControl,
+#                          metric = "ROC")
+# )
 
-system.time(
-  rf_model_smote <- train(Class ~ .,
-                         data = train_data,
-                         method ='rf',
-                         tuneLength = 5,
-                         trControl = fitControl,
-                         metric = "AUC")
-)
-
-# Train a Random Forest model with ROSE during resample
+# # Train a Random Forest model with ROSE during resample
 set.seed(3219)
 fitControl$sampling = "rose"
 
@@ -447,25 +507,27 @@ system.time(
                          method ='rf',
                          tuneLength = 5,
                          trControl = fitControl,
-                         metric = "AUC")
+                         metric = "ROC")
 )
 
 
 
 
-print(rf_model) #print the trained model summary
-train_plot <- plot(rf_model) #plot the Kappa scores for the different ntree and mtry combinations
+print(rf_model_rose) #print the trained model summary
+train_plot <- plot(rf_model_rose) #plot the Kappa scores for the different ntree and mtry combinations
 print(train_plot)
 
-rf_pred <- predict(rf_model, test_data) #apply trained random forest model to test data
-rf_cm <- confusionMatrix(rf_pred, test.a$outcome)
-rf_sens <- sensitivity(rf_pred, test.a$outcome)
-rf_spec <- specificity(rf_pred, test.a$outcome)
+rf_pred <- predict(rf_model_rose, test_data) #apply trained random forest model to test data
+rf_cm <- confusionMatrix(rf_pred, test_label)
+rf_sens <- sensitivity(rf_pred, test_label)
+rf_spec <- specificity(rf_pred, test_label)
 
-rf_labels <- ifelse(test.a$outcome == "Fraud", 1, 0)
+rf_labels <- ifelse(test_label == "Fraud", 1, 0)
 rf_predictions <- ifelse(rf_pred == "Fraud", 1, 0)
 rf_roc <- roc(rf_labels, rf_predictions)
 rf_auc <- auc(rf_roc)
+print(rf_cm)
+print(rf_auc)
 
 #plot the top 20 most importance variables in the RF model
 var_imp_plot <- plot(varImp(rf_model, scale = FALSE), top = 20)
@@ -475,6 +537,47 @@ print(var_imp_plot)
 # ======================================================
 # Model Development - XGBoost
 # ======================================================
+xgb_imblance_handling <- "smote" # use either "smote", "rose", "under", "over"
+
+# Split the data4 data frame into train and test sets
+gb_train1 <- data4[train_index, ]
+gb_test1 <- data4[!(as.numeric(rownames(data4)) %in% train_index), ]
+
+# isolate the numeric variables of both the train and test sets
+num_fields <- colnames(gb_train1[sapply(gb_train1, is.numeric)])
+gb_train_num1 <- gb_train1[, num_fields]
+gb_test_num1 <- gb_test1[, num_fields]
+
+#Impute the values of numeric variables with the mean of the training set values
+for(i in 1:ncol(gb_train_num1)) {
+  gb_train_num1[is.na(gb_train_num1[, i]), i] <- mean(gb_train_num1[,i], na.rm = TRUE)
+}
+
+for(i in 1:ncol(gb_test_num1)) {
+  gb_test_num1[is.na(gb_test_num1[, i]), i] <- mean(gb_train_num1[,i], na.rm = TRUE)
+}
+
+# Center and scale both the train and test sets,
+# based on the center and scale of the train set
+scale_process <- preProcess(gb_train_num1, method = c("center", "scale"))
+stand_train1 <- predict(scale_process, gb_train_num1)
+stand_test1 <- predict(scale_process, gb_test_num1)
+
+# Combine the newly-scaled numeric and categorical training variables back together
+gb_train2 <- cbind(gb_train_num1, gb_train1[, !(colnames(gb_train1) %in% num_fields)])
+
+# Down-sample the majority class of the training data
+
+
+# Use SMOTE to synthetically balance the two classes in the training data
+gb_train <- SMOTE(Class ~ .,
+                  gb_train2,
+                  perc.over = 500,
+                  perc.under = 200)
+table(gb_train$Class)
+
+# Combine the newly-scaled numeric and categorical testing variables back together
+gb_test <- cbind(gb_test_num1, gb_test1[, !(colnames(gb_test1) %in% num_fields)])
 
 #create sparse matrix for numeric predictors
 M.a <- sparse.model.matrix(~ creditLimit +
@@ -482,9 +585,130 @@ M.a <- sparse.model.matrix(~ creditLimit +
                              transactionAmount +
                              currentBalance +
                              time_from_previous +
+                             total_trans_count +
+                             time_of_day +
                              CustTimeZscore +
-                             CustAmtZscore + -1, data = rbind(train_data %>% select(-outcome),
-                                                              test_data))
+                             CustAmtZscore +
+                             category_trans +
+                             category_dollars +
+                             total_dollars +
+                             perc_cat_trans +
+                             perc_cat_dollars +
+                             hour_trans +
+                             hour_dollars +
+                             perc_hour_trans +
+                             perc_hour_dollars -1,
+                           data = rbind(gb_train[, num_fields],
+                                        gb_test[, num_fields]))
+
+#create sparse matrix for categorical predictors
+#################################################
+cats_train <- gb_train[, !(colnames(gb_train) %in% num_fields)] %>%
+  select(-Class)
+cats_test <- gb_test[, !(colnames(gb_test) %in% num_fields)] %>%
+  select(-Class)
+
+cats <- data.table(rbind(cats_train, cats_test))
+cats$account <- c(1:nrow(cats))
+
+#identify unique categorical feature values for each account (record)
+d1 <- cats[, list(account, acqCountry)]
+d2 <- cats[, list(account, merchantCountryCode)]
+d3 <- cats[, list(account, posEntryMode)]
+d4 <- cats[, list(account, posConditionCode)]
+d5 <- cats[, list(account, merchantCategoryCode)]
+d6 <- cats[, list(account, transactionType)]
+d7 <- cats[, list(account, cardPresent)]
+d8 <- cats[, list(account, expirationDateKeyInMatch)]
+d9 <- cats[, list(account, hour_of_day)]
+
+d1[ , acqCountry:= paste0("acqCountry: ", acqCountry)]
+d2[ , merchantCountryCode:= paste0("merchantCountryCode: ", merchantCountryCode)]
+d3[ , posEntryMode:= paste0("posEntryMode: ", posEntryMode)]
+d4[ , posConditionCode:= paste0("posConditionCode: ", posConditionCode)]
+d5[ , merchantCategoryCode:= paste0("merchantCategoryCode: ", merchantCategoryCode)]
+d6[ , transactionType:= paste0("transactionType: ", transactionType)]
+d7[ , cardPresent:= paste0("cardPresent: ", cardPresent)]
+d8[ , expirationDateKeyInMatch:= paste0("expirationDateKeyInMatch: ", expirationDateKeyInMatch)]
+d9[ , hour_of_day:= paste0("hour_of_day: ", hour_of_day)]
+
+names(d1) <- names(d2) <- names(d3) <- names(d4) <- names(d5) <- names(d6) <- names(d7) <- names(d8) <- c("account","feature_name")
+d <- rbind(d1, d2, d3, d4, d5, d6, d7, d8)
+rm(d1, d2, d3, d4, d5, d6, d7, d8); gc()
+d <- unique(d)
+setkey(d, account)
+
+#creates a list of unique accounts (records)
+ii <- as.character(unique(d$account))
+#creates a list of all unique feature_names
+jj <- unique(d$feature_name)
+#creates a list the length of dd that gives each account a unique identifier from 1: the number of unique accounts
+id_i <- match(d$account,ii)
+#same thing for feature_name
+id_j <- match(d$feature_name,jj)
+id_ij <- cbind(id_i,id_j)
+#creates a matrix frame that has the feature_names as column names and accounts as row names, and every point is blank
+M.b <- Matrix(0,nrow=length(ii),ncol=length(jj),
+              dimnames=list(ii,jj),sparse=T)
+#if the account and feature_name are found together in the id_i data frame, then mark it as a 1 in the M.b matrix
+M.b[id_ij] <- 1
+rm(ii,jj,id_i,id_j,id_ij);gc()
+
+#combine the numeric and categorical matrices
+M <- cbind(M.a, M.b)
+
+#create xgb matrices for the xgboost model
+#################################################
+train_data <- M[1:nrow(gb_train), ]
+trwr <- sample(1:nrow(train_data), round(.85*nrow(train_data), 0), replace = FALSE)
+trw_data <- train_data[trwr, ]
+tew_data <- train_data[-trwr, ]
+
+test_data <- M[(nrow(gb_train)+1):nrow(M), ]
+
+trw_label <- ifelse(gb_train[trwr, ]$Class == "Fraud", 1, 0)
+tew_label <- ifelse(gb_train[-trwr, ]$Class == "Fraud", 1, 0)
+test_label <- ifelse(gb_test$Class == "Fraud", 1, 0)
+
+dtrain_tr <- xgb.DMatrix(data = trw_data, label = trw_label)
+dtrain_te <- xgb.DMatrix(data = tew_data, label = tew_label)
+dtest <- xgb.DMatrix(data = test_data, label = test_label)
+
+#train xgboost tree model
+##############################
+
+watchlist <- list(train = dtrain_tr, test = dtrain_te)
+bst <- xgb.train(data = dtrain_tr,
+                 #watchlist = watchlist,
+                 eta = .1,
+                 nround = 200,
+                 booster = "gbtree",
+                 objective = "binary:logistic",
+                 eval_metric = "auc")
+
+
+gb_pred <- as.factor(ifelse(predict(bst, tew_data) >= .5, "Fraud", "NotFraud")) #apply trained xgboost model to test data
+test_label_comp <- as.factor(ifelse(tew_label == 1, "Fraud", "NotFraud"))
+gb_cm <- confusionMatrix(gb_pred, test_label_comp)
+
+gb_predictions <- ifelse(gb_pred == "Fraud", 1, 0)
+gb_roc <- roc(tew_label, gb_predictions)
+gb_auc <- auc(gb_roc)
+print(gb_auc)
+
+
+#perform prediction
+gb_pred <- as.factor(ifelse(predict(bst, test_data) >= .5, "Fraud", "NotFraud")) #apply trained xgboost model to test data
+test_label_comp <- as.factor(ifelse(test_label == 1, "Fraud", "NotFraud"))
+gb_cm <- confusionMatrix(gb_pred, test_label_comp)
+print(gb_cm)
+gb_sens <- sensitivity(gb_pred, test_label_comp)
+gb_spec <- specificity(gb_pred, test_label_comp)
+
+gb_predictions <- ifelse(gb_pred == "Fraud", 1, 0)
+gb_roc <- roc(test_label, gb_predictions)
+gb_auc <- auc(gb_roc)
+print(gb_auc)
 
 
 
